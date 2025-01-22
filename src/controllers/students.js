2252dc68385e3923c5fb8c1458e6803157ac97b0
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2025-01-22 13:08:51 +0100

    Initial commit

---
+++ src/controllers/students.js
@@ -0,0 +1,27 @@
+import { getAllStudents, getStudentById } from '../services/students';
+import createHttpError from 'http-errors';
+export const getStudentsController = async (req, res, next) => {
+  try {
+    const students = await getAllStudents();
+
+    res.json({
+      status: 200,
+      message: 'Successfully found students!',
+      data: students,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+export const getStudentByIdController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+  if (!student) {
+    throw createHttpError(404, 'Student not found');
+  }
+  res.json({
+    status: 200,
+    message: `Successfully found student with id ${studentId}!`,
+    data: student,
+  });
+};
